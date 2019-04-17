57ea5d49f2f6711c3661c2320e9db0b63bd85641
Mego1596 <maurigranados1596@gmail.com>
2019-04-17 10:06:11 -0600

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -67,7 +67,7 @@
       <!-- Nav Item - Dashboard -->
       <li class="nav-item">
         <a class="nav-link" href="index.html">
-          <i class="fa fa-home"></i>
+          <i class="fas fa-fw fa-home"></i>
           <span>Inicio</span></a>
       </li>
 
@@ -82,34 +82,29 @@
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
+          <i class="fas fa-fw fa-book"></i>
+          <span>Reportes Tacticos</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <h6 class="collapse-header">Reportes Tacticos:</h6>
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Productos</h6>
+            <div class="collapse-divider"></div>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 1</i></a>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 2</i></a>
+            <h6 class="collapse-header">Materia Prima</h6>
+            <div class="collapse-divider"></div>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 3</i></a>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 4</i></a>
+            <h6 class="collapse-header">Clientes</h6>
+            <div class="collapse-divider"></div>
+            <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 5</i></a>
+            <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 6</i></a>
           </div>
         </div>
       </li>
 
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
-          </div>
-        </div>
-      </li>
 
       <!-- Divider -->
       <hr class="sidebar-divider">
@@ -123,39 +118,42 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
+          <span>Reportes Estrategicos</span>
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Login Screens:</h6>
-            <a class="collapse-item" href="login.html">Login</a>
-            <a class="collapse-item" href="register.html">Register</a>
-            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+            <h6 class="collapse-header">Reportes Estrategicos:</h6>
             <div class="collapse-divider"></div>
-            <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item" href="404.html">404 Page</a>
-            <a class="collapse-item" href="blank.html">Blank Page</a>
+            <h6 class="collapse-header">Categorias de Producto</h6>
+            <div class="collapse-divider"></div>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 1</i></a>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 2</i></a>
+            <h6 class="collapse-header">Materia Prima</h6>
+            <div class="collapse-divider"></div>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 3</i></a>
+              <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 4</i></a>
+            <h6 class="collapse-header">Clientes</h6>
+            <div class="collapse-divider"></div>
+            <a class="collapse-item" href=""><i class="fa fa-file-text"></i> Reporte 5</i></a>
           </div>
         </div>
       </li>
 
-      <!-- Nav Item - Charts -->
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li>
-
-      <!-- Nav Item - Tables -->
-      <li class="nav-item active">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-      </li>
-
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
-
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-cogs"></i>
+          <span>Usuarios</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Acciones:</h6>
+            <a class="collapse-item" href=""><i class="fa fa-group"> Lista de Usuarios</i></a>
+            <a class="collapse-item" href=""><i class="fa fa-archive"> Actividad de usuarios</i></a>
+          </div>
+        </div>
+      </li>
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
