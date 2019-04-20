a7ffab4193a30f0f4a0cef1373024884cbc3fb70
alegranados <alegranados03@gmail.com>
2019-04-20 00:36:56 -0600

    instalacion a mac
    alteracion de permisos de los archivos en mac para funcionamiento correcto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,14 @@ class HomeController extends Controller
     public function index()
     {
         if(Auth::user()->isAdmin()){
-            $sqlQuery = "SELECT CONCAT(u.primer_nombre, ' ', u.segundo_nombre,' ',u.primer_apellido,' ',u.segundo_apellido) as nombre_usuario, u.email as email, r.name as nombre_rol FROM users as u inner join role_user on role_user.user_id=u.id inner join roles as r on role_user.role_id=r.id;";
+            $sqlQuery = "SELECT 
+            CONCAT(u.primer_nombre, ' ', u.segundo_nombre,' ',u.primer_apellido,' ',u.segundo_apellido) as nombre_usuario, 
+            u.email as email,
+             r.name as nombre_rol 
+            FROM users as u 
+            inner join role_user on role_user.user_id=u.id 
+            inner join roles as r on role_user.role_id=r.id;";
+            
             $usuarios = DB::select(DB::raw($sqlQuery));
             return view('usuario.index',compact('usuarios'));
         }
