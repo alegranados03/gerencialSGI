4f8f6cb794c57d4723d8c5f397a8da906c741b05
NardoXD <lopezcesar292@gmail.com>
2019-04-20 23:19:30 -0600

    Script de ETL agregado

---
+++ Python-ETL/script_ETL.py
@@ -0,0 +1,73 @@
+import mysql.connector
+
+lista_resultados = list()
+
+"""Listado de tablas, campos de la BD transaccional y campos de la BD gerencial"""
+tablas_trans = [
+    'users', 'ordenes', 'pagos', 'categorias', 'productos',
+    'detalles_orden', 'materia_prima', 'proveedores', 'lote', 'compras'
+]
+
+tablas_geren = [
+    'usuario', 'orden', 'pago', 'categoria', 'producto',
+    'detalle_orden', 'materia_prima', 'proveedor', 'lote', 'compra'
+]
+
+campos_trans = [
+    ('id', 'username', 'es_cliente', 'sexo', 'created_at'),
+    ('id', 'tipo_orden', 'user_id', 'created_at'),
+    ('id', 'orden_id', 'tipo_pago', 'total_cancelar', 'created_at'),
+    ('id', 'nombre_categoria'),
+    ('id', 'nombre_producto', 'categoria_id', 'precio'),
+    ('id', 'orden_id', 'producto_id', 'cantidad_producto', 'total_parcial', 'created_at'),
+    ('id', 'nombre_materia', 'cantidad'),('id', 'nombre_proveedor'),
+    ('id', 'producto_id', 'total', 'proveedor_id', 'created_at'),
+    ('id', 'materia_prima_id', 'proveedor_id', 'cantidad', 'costo_compra', 'created_at')
+]
+
+"""Obtención de campos de la BD transaccional."""
+
+"""
+Consulta de iteraciones a la BD transaccional
+
+Primeramente se crea la conexion a la BD transaccional.
+Se crea el cursor a utilizar para el manejo de resultados.
+Durante cada ciclo se crea una nueva query formada a partir de los campos de cada tabla, y su nombre.
+Se almacenan en la variable de lista_resultados.
+Se cierra la conexion del cursor.
+"""
+mydb_trans = mysql.connector.connect(host='localhost', user='fernando',
+                                     passwd='Fernando1102', database='transaccional_sgi')
+
+mycursor = mydb_trans.cursor()
+
+for i in range(len(tablas_trans)):
+    query = 'SELECT {} FROM {}'.format(','.join(campos_trans[i]), tablas_trans[i])
+    mycursor.execute(query)
+    lista_resultados.append(mycursor.fetchall())
+mycursor.close()
+
+"""
+Carga de valores a la base de datos
+
+Primeramente se crea la conexion a la BD gerencial.
+Se crea el cursor a utilizar para el manejo de resultados.
+Para cada ciclo se crea un formato de query.
+Se ejecuta la query con sus respectivos datos.
+Se cierra el cursor
+Se confirman los cambios en la BD gerencial
+"""
+
+mydb_geren = mydb_trans = mysql.connector.connect(host='localhost', user='fernando',
+                                                  passwd='Fernando1102', database='gerencialpan')
+mycursor = mydb_geren.cursor()
+
+for i in range(len(tablas_geren)):
+    query = 'INSERT INTO {} VALUES ({})'.format(
+        'gerencial_' + tablas_geren[i],
+        ','.join(['%s' for i in range(len(lista_resultados[i][0]))])
+    )
+    mycursor.executemany(query, lista_resultados[i])
+
+mycursor.close()
+mydb_geren.commit()
