5b547eaa8bcf1fb4978072e1d5eb7df74696344e
alegranados03 <alegranados03@gmail.com>
2019-04-20 20:41:18 -0600

    cambio historial
    eliminacion de una tabla

--- database/migrations/2019_04_12_023244_create_historial_actividad_table.php
+++ database/migrations/2019_04_12_023244_create_historial_actividad_table.php
@@ -18,6 +18,7 @@ class CreateHistorialActividadTable extends Migration
             $table->integer('user_id')->unsigned()->nullable()->comment('id del usuario del sistema al que está relacionada la actividad');
             $table->foreign('user_id')->references('id')->on('users');
             $table->boolean('registro_etl')->nullable(0)->comment('identifica si la actividad es del sistema ETL (estado True) o de un usuario del sistema gerencial (estado False)');
+            $table->string('comentario_de_actividad')->comment('Registra la acción que ha realizado el usuario');
             $table->timestamps();
         });
     }
